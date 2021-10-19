052b2ee97caae34a15bb02ab5289a81f89acf254
xTheCoderx <justaguyoney@gmail.com>
2021-10-19 15:34:20 +1300

    Transitioned code from MapsActivity.kt (deleted) to MainActivity.kt.

--- app/src/main/java/com/realtomjoney/mapsapp/MainActivity.kt
+++ app/src/main/java/com/realtomjoney/mapsapp/MainActivity.kt
@@ -2,10 +2,29 @@ package com.realtomjoney.mapsapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.realtomjoney.mapsapp.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+    private lateinit var binding: ActivityMainBinding
+
+    lateinit var myMap: GoogleMap
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
     }
+
+    override fun onMapReady(p0: GoogleMap) {
+        myMap = p0
+    }
+
 }
