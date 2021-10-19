8b240da7f0609cf6f9d2008c9e86935db1ec1116
xTheCoderx <justaguyoney@gmail.com>
2021-10-19 18:03:25 +1300

    Added markers to the map.

--- app/src/main/java/com/realtomjoney/mapsapp/MainActivity.kt
+++ app/src/main/java/com/realtomjoney/mapsapp/MainActivity.kt
@@ -2,9 +2,12 @@ package com.realtomjoney.mapsapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import com.realtomjoney.mapsapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity(), OnMapReadyCallback {
@@ -21,10 +24,24 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+        binding.button.setOnClickListener {
+            val coordinates = LatLng(49.284670, -123.121539)
+
+            val markerOptions: MarkerOptions =
+                MarkerOptions().position(coordinates).title("Downtown Vancouver")
+
+            myMap.addMarker(markerOptions)
+            myMap.animateCamera(CameraUpdateFactory.newLatLngZoom(coordinates, 12.0f))
+        }
     }
 
     override fun onMapReady(p0: GoogleMap) {
         myMap = p0
+        myMap.setOnMarkerClickListener { marker ->
+            marker.showInfoWindow()
+            true
+        }
     }
 
 }
